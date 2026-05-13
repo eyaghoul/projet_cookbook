f70f37ae561bc1c090f68b2f8c7a78e67de387b2
eyaghoul <eya.ghoul@polytechnicien.tn>
2026-05-13 13:24:36 +0100

    second commit

---
+++ Models/IngredientRow.cs
@@ -0,0 +1,8 @@
+﻿namespace CookBook.Models;
+
+public class IngredientRow
+{
+    public string IngredientName { get; set; } = "";
+    public double Quantity { get; set; }
+    public string UnitAbbreviation { get; set; } = "";
+}
